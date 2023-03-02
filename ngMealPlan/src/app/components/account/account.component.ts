0bce3256240c59de672e6b552164f660ae2b2b6f
Liz <lizbaze4730@gmail.com>
2023-03-02 17:44:01 -0600

    Moving management of a user's recipes to account page

--- ngMealPlan/src/app/components/account/account.component.ts
+++ ngMealPlan/src/app/components/account/account.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/models/recipe';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
 })
-export class AccountComponent {
+export class AccountComponent implements OnInit {
+  recipes: Recipe[] | null = null;
+  selected: Recipe | null = null;
+  user: User | null = null;
+  editRecipe: Recipe | null = null;
 
+  constructor(private auth: AuthService) {}
+
+  ngOnInit() {
+    if (this.loggedIn()) {
+      this.getUser();
+    }
+  }
+
+  loggedIn(): boolean {
+    return this.auth.checkLogin();
+  }
+
+  getUser() {
+    this.auth.getLoggedInUser().subscribe({
+      next: (user: User) => {
+        this.user = user;
+      },
+      error: (err: any) => {
+        console.error(err);
+      },
+    });
+  }
+
+  myRecipes() {
+    if (this.user) {
+      this.recipes = this.user.recipes;
+    }
+  }
+
+  selectRecipe(recipe:Recipe) {
+    this.selected = recipe;
+  }
+
+  selectRecipeToEdit(recipe: Recipe) {
+    this.editRecipe  = recipe;
+  }
 }
