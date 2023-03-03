3e13fb1430e41c54fe65497968950c41771d19e3
Liz <lizbaze4730@gmail.com>
2023-03-03 15:43:31 -0600

    Add styling to register/login component

--- ngMealPlan/src/app/components/register/register.component.ts
+++ ngMealPlan/src/app/components/register/register.component.ts
@@ -6,45 +6,51 @@ import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-
   user: User = new User();
   logInUser: User = new User();
-
+  confirmPassword: string = '';
+  passwordMatch: boolean = true;
+  loginFailure = false;
 
   constructor(private auth: AuthService, private router: Router) {}
 
-
   register(user: User) {
-    this.auth.register(user).subscribe({
-      next: (regUser: User) => {
-        this.auth.login(user.username, user.password).subscribe({
-          next: (loggedInUser) => {
-            console.log("it worked")
-          },
-          error: (problem) => {
-            console.error('RegisterComponent.register(): Error logging in user:');
-            console.error(problem);
-          }
-        });
-        this.user = new User();
-      }
-    })
+    if (user.password !== this.confirmPassword) {
+      this.passwordMatch = false;
+    } else {
+      this.auth.register(user).subscribe({
+        next: (regUser: User) => {
+          this.auth.login(user.username, user.password).subscribe({
+            next: (loggedInUser) => {
+              this.confirmPassword = "";
+              this.router.navigateByUrl("/home");
+            },
+            error: (problem) => {
+              console.error(
+                'RegisterComponent.register(): Error logging in user:'
+              );
+              console.error(problem);
+            },
+          });
+          this.user = new User();
+        },
+      });
+    }
   }
 
   logIn(username: string, password: string) {
     this.auth.login(username, password).subscribe({
       next: (loggedInUser) => {
-        console.log("it worked")
+
       },
       error: (problem) => {
         console.error('RegisterComponent.register(): Error logging in user:');
         console.error(problem);
-      }
-    })
+        this.loginFailure = true;
+      },
+    });
   }
-
-
 }
