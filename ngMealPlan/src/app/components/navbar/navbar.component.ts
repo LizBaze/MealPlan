1bc17638c64a2bb7eb89b38a129c30beececc7b6
Liz <lizbaze4730@gmail.com>
2023-03-08 17:38:52 -0600

    Navbar links now reload the page and reset the view for better user experience

--- ngMealPlan/src/app/components/navbar/navbar.component.ts
+++ ngMealPlan/src/app/components/navbar/navbar.component.ts
@@ -5,21 +5,23 @@ import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
-
   constructor(private auth: AuthService, private router: Router) {}
+
+  redirectTo(uri: string) {
+    this.router
+      .navigateByUrl('/', { skipLocationChange: true })
+      .then(() => this.router.navigate([uri]));
+  }
 
   loggedIn(): boolean {
     return this.auth.checkLogin();
   }
 
-  logout(){
-    this.auth.logout()
-    this.router.navigateByUrl("home");
+  logout() {
+    this.auth.logout();
+    this.router.navigateByUrl('home');
   }
-
-
-
 }
